a7a909722d7c8812bf33e2ef11e65382e4170779
Rohit Tokala <rtokala@purdue.edu>
2023-02-22 00:45:01 -0500

    Flesh out note editor and cote cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { useSession } from "next-auth/react";
 import { api, type RouterOutputs } from "~/utils/api";
 import { Header } from "~/components/Header";
 import React, { useState } from "react";
+import { NoteEditor } from "~/components/NoteEditor";
+import { NoteCard } from "~/components/NoteCard";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -49,6 +51,27 @@ const Content: React.FC = () => {
     },
   });
 
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
   return (
     <div className=" mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className=" px-2">
@@ -82,7 +105,27 @@ const Content: React.FC = () => {
           }}
         />
       </div>
-      <div className=" col-span-3"></div>
+      <div className=" col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
+          }}
+        />
+      </div>
     </div>
   );
 };
